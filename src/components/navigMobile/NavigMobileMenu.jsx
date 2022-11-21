6e2c42a694190bb2c27720a9a3dd14c022952939
Miguel Villanueva <emvil542@gmail.com>
2022-11-21 14:21:38 +0800

    feat(mobile): add dropdown styling

--- src/components/navigMobile/NavigMobileMenu.jsx
+++ src/components/navigMobile/NavigMobileMenu.jsx
@@ -7,15 +7,63 @@ const NavigMobileMenu = () => {
     return (
         <div className={B()}>
             <div>
-            <section className={E('dropdown')}>
-                <h2>Features</h2>
-                <img src="images/icon-arrow-down.svg" alt="" />
-            </section>
+            <div className={E('dropdown-wrap')}>
+                <section className={E('dropdown')}>
+                    <h2>Features</h2>
+                    <img src="images/icon-arrow-down.svg" alt="" />
+                </section>
+                
+                <menu>
+                    <li>
+                        <div className={E('icon-wrap')}>
+                            <img src="images/icon-todo.svg" alt="" />
+                        </div>
+                        Todo List
+                    </li>
+
+                    <li>
+                        <div className={E('icon-wrap')}>
+                            <img src="images/icon-calendar.svg" alt="" />
+                        </div>
+                        Calendar
+                    </li>
+
+                    <li>
+                        <div className={E('icon-wrap')}>
+                            <img src="images/icon-reminders.svg" alt="" />
+                        </div>
+                        Reminders
+                    </li>
+
+                    <li>
+                        <div className={E('icon-wrap')}>
+                            <img src="images/icon-planning.svg" alt="" />
+                        </div>
+                        Planning
+                    </li> 
+                </menu>
+            </div>
             
-            <section  className={E('dropdown')}>
-                <h2>Company</h2>
-                <img src="images/icon-arrow-down.svg" alt="" />
-            </section>
+            <div className={E('dropdown-wrap')}>
+                <section  className={E('dropdown')}>
+                    <h2>Company</h2>
+                    <img src="images/icon-arrow-down.svg" alt="" />
+                </section>
+
+                <menu>
+                    <li>
+                        History
+                    </li>
+
+                    <li>
+                        Our Team
+                    </li>
+
+                    <li>
+                        Blog
+                    </li>
+                </menu>
+            </div>
             
             <h2>Careers</h2>
             <h2>About</h2>
